6e7bba04d6f0d67f1c5c511ccd50a202baa29f39
Supinder Singh <supinders39@gmail.com>
2023-11-26 17:12:37 +0530

    logo updated

--- components/AuthModal.tsx
+++ components/AuthModal.tsx
@@ -37,7 +37,7 @@ const AuthModal: FC<AuthModalProps> = ({ }) => {
         >
             <Auth
                 theme='dark'
-                providers={["google", "facebook", "github"]}
+                providers={["google", "github"]}
                 magicLink
                 supabaseClient={supabaseClient}
                 appearance={{
